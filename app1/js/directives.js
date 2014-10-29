d2073477559ce1161395368ddb907f6863f0abec
crysthianophp <crysthianophp@gmail.com>
2014-10-29 00:41:23 -0200

    estudos em casa dia 28

--- app1/js/directives.js
+++ app1/js/directives.js
@@ -1,31 +1,14 @@
 stock.directive("stockWidget", [function() {
 	return {
-		restrict: 'AE',
+		restrict: 'A',
 		templateUrl: 'stock.html',
+		transclude: true,
 		scope: {
-			stockData: '=',
-			stockTitle: '@',
-			whenSelect: '&'
+			stockData: '='
 		},
 		link: function($scope, $element, $attrs) {
 			$scope.getChange = function(stock) {
 				return Math.ceil(((stock.price - stock.previous) / stock.previous) * 100);
-			};
-
-			$scope.changeStock = function() {
-				$scope.stockData = {
-					name: 'Directive Stock',
-					price: 500,
-					previous: 200
-				};
-			};
-
-			$scope.onSelect = function() {
-				$scope.whenSelect({
-					stockName: $scope.stockData.name,
-					stockPrice: $scope.stockData.price,
-					stockPrevious: $scope.stockData.previous
-				});
 			};
 		}
 	}
